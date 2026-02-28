8da42ca8ab8eba90b600a3c4b3c4fbe6663c3799
Jago de Vreede <jagodevreede@gmail.com>
2026-02-28 09:47:50 +0100

    STEP 6 Added spans

--- greeting/src/main/java/org/acme/GreetingProvider.java
+++ greeting/src/main/java/org/acme/GreetingProvider.java
@@ -1,6 +1,7 @@
 package org.acme;
 
 import io.micrometer.core.annotation.Timed;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -9,6 +10,7 @@ public class GreetingProvider {
     @ConfigProperty(name = "message")
     String message;
 
+    @WithSpan
     @Timed("MyGreeting")
     public String getGreeting() {
         return message;
--- greeting/src/main/java/org/acme/GreetingResource.java
+++ greeting/src/main/java/org/acme/GreetingResource.java
@@ -1,12 +1,19 @@
 package org.acme;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Path("/hello")
 @Produces(MediaType.TEXT_PLAIN)
@@ -21,7 +28,11 @@ public class GreetingResource {
 
     @GET
     public String hello() {
-        return greetingProvider.getGreeting();
+        var greeting = greetingProvider.getGreeting();
+        String password = IntStream.range(0, 10)
+                .mapToObj(x -> rndChar())
+                .collect(Collectors.joining(""));
+        return greeting + " your pasword is " + password;
     }
 
     @GET
@@ -31,11 +42,19 @@ public class GreetingResource {
     }
 
     @POST
+    @WithSpan
     public Response createGreeting(String text) {
         Greeting greeting = new Greeting();
         greeting.setText(text);
         greeting.setId(UUID.randomUUID());
         greetingRepository.persist(greeting);
         return Response.ok(greeting.getId()).build();
+    }
+
+    @WithSpan
+    String rndChar () {
+        int rnd = (int) (Math.random() * 52);
+        char base = (rnd < 26) ? 'A' : 'a';
+        return String.valueOf(base + rnd % 26);
     }
 }
